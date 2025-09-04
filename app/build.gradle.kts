5d6b1e6bac20ca3bf5d438bd9d0761313f46c3ac
Keval Bhanushali <mrbhanushali03@gmail.com>
2025-09-04 20:25:35 +0530

    shoes detail page created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.viewpager2)
     implementation(libs.core.ktx)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
